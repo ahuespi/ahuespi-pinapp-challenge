9bdae373ef0142356ac5c0d33383e042a913da71
ahuespi <amir.huespi@gmail.com>
2023-08-08 01:31:37 -0300

    feat: Traigo la información desde Firebase, muestro el listado de usuarios, hago el porcentaje de edad y la desviacion estandar, estilizo los componente, creo el modal para agregar clientes, y hago el post a la DB

---
+++ scripts/firebase.setting.js
@@ -0,0 +1,18 @@
+// Configuración de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwNYPNspvBntINPES6RyK3hXqfy0X3cnQ",
+    authDomain: "ahuespi-pinapp-challenge.firebaseapp.com",
+    projectId: "ahuespi-pinapp-challenge",
+    storageBucket: "ahuespi-pinapp-challenge.appspot.com",
+    messagingSenderId: "809292502601",
+    appId: "1:809292502601:web:dd04b43cdacc072c65d198"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// Fin de la configuración de Firebase
+
+export { db, collection, addDoc, getDocs }
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,80 @@
-// Configuración de Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { db, collection, addDoc, getDocs } from "./firebase.setting.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCwNYPNspvBntINPES6RyK3hXqfy0X3cnQ",
-    authDomain: "ahuespi-pinapp-challenge.firebaseapp.com",
-    projectId: "ahuespi-pinapp-challenge",
-    storageBucket: "ahuespi-pinapp-challenge.appspot.com",
-    messagingSenderId: "809292502601",
-    appId: "1:809292502601:web:dd04b43cdacc072c65d198"
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-// Fin de la configuración de Firebase
-
-// Obtengo todos los pacientes de la DB
+// Obtengo todos los clientes de la DB
 const getClients = async () => {
-    const clients = await getDocs(collection(db, "clients"));
-    const listClients = clients.docs.map((list) => list.data());
-    console.log(listClients)
+    const getClients = await getDocs(collection(db, "clients"));
+    const clients = getClients.docs.map((list) => list.data());
+    const allAges = getClients.docs.map((client) => Number(client.data().age));
+
+    // Muestro la lista de clientes
+    clients.forEach((client) => {
+        $('#clientList').append(`
+            <div class="shadow-xl border rounded px-5 py-10 mt-2">
+                <h5 class="font-bold text-xl">${client.firstName} ${client.lastName}</h5>
+                <p >Nacimiento: ${client.birthday} (${client.age} años)</p>
+            </div>            
+        `)
+    })
+
+    // Muestro el promedio de edad
+    const averageAges = calculateAverage(allAges)
+    $('#averageAges').html(`${averageAges} años`)
+
+    // Muestro la desviación
+    const deviation = calculateDeviation(allAges, averageAges)
+    $('#deviation').html(`${deviation}`)
 }
 
+// Calculo el promedio de edad
+const calculateAverage = (ages) => {
+    const averageCount = ages.reduce((prev, age) => prev + age, 0) / ages.length;
+    return averageCount.toFixed(2);
+}
 
-getClients()
+// Calculo la desviacion estandar
+const calculateDeviation = (ages, average) => {
+    const getNominator = ages.map((i) => {
+        const sumAges = i - average;
+        const powAges = Math.pow(sumAges, 2);
+
+        return powAges;
+    })
+
+    const sumNominatorValues = getNominator.reduce((prev, age) => prev + age, 0);
+    const getDenominator = ages.length - 1;
+
+    const standarDeviation = Math.sqrt(sumNominatorValues / getDenominator);
+
+    return standarDeviation.toFixed(2);
+}
+
+$('#openModal').on('click', () => {
+    alert('Amir')
+})
+
+const addClient = (e) => {
+    e.preventDefault();
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const age = $('#age').val();
+    const birthday = $('#birthday').val();
+
+    try {
+        const docRef = addDoc(collection(db, "clients"), {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            birthday: birthday
+        });
+        $('#form-success').show();
+        console.log('Se envió')
+    } catch (e) {
+        $('#form-danger').show();
+        console.log(e)
+    }
+}
+
+$(document).ready(() => {
+    getClients()
+    $('#addClient').on('submit', addClient);
+})
