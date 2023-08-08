20d195909e4d7c4a46a618de0b84647b8ba4834c
ahuespi <amir.huespi@gmail.com>
2023-08-08 11:08:43 -0300

    feat: Estilizo el form, agrego el nav, finalizo maquetacion mobile

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,35 @@ const getClients = async () => {
     const getClients = await getDocs(collection(db, "clients"));
     const clients = getClients.docs.map((list) => list.data());
     const allAges = getClients.docs.map((client) => Number(client.data().age));
+    $('.client__user').remove()
 
     // Muestro la lista de clientes
     clients.forEach((client) => {
         $('#clientList').append(`
-            <div class="shadow-xl border rounded px-5 py-10 mt-2">
-                <h5 class="font-bold text-xl">${client.firstName} ${client.lastName}</h5>
-                <p >Nacimiento: ${client.birthday} (${client.age} años)</p>
+            <div class="shadow-xl border rounded px-5 py-10 mt-2 client__user flex">
+                <div class="flex items-center justify-center bg-orange-50 h-12 w-12 rounded-full border border-orange-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-orange-400" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
+                </div>
+                <div class="pl-5">
+                    <h5 class="font-bold text-xl">${client.firstName} ${client.lastName}</h5>
+                    <p >Nacimiento: ${client.birthday} (${client.age} años)</p>
+                </div>
+                
             </div>            
         `)
     })
 
     // Muestro el promedio de edad
     const averageAges = calculateAverage(allAges)
-    $('#averageAges').html(`${averageAges} años`)
+    if (isNaN(averageAges)) {
+        $('#averageAges').html(`No hay datos`)
+    } else {
+        $('#averageAges').html(`${averageAges} años`)
+    }
 
     // Muestro la desviación
     const deviation = calculateDeviation(allAges, averageAges)
@@ -49,32 +64,54 @@ const calculateDeviation = (ages, average) => {
 }
 
 $('#openModal').on('click', () => {
-    alert('Amir')
+    $('#formAddClient').fadeIn(250)
+    $('#sendButton').html('Crear cliente')
+    $('#sendButton').css({
+        "color": 'white',
+        "font-weight": 'bold',
+        "background-color": "rgb(14 165 233 / 1)"
+    })
+})
+$('#closeModal').on('click', () => {
+    $('#formAddClient').fadeOut(250)
 })
 
 const addClient = (e) => {
     e.preventDefault();
+    $('#sendButton').attr('disabled', 'disabled')
+    $('#sendButton').html('Creando...')
     const firstName = $('#firstName').val();
     const lastName = $('#lastName').val();
     const age = $('#age').val();
     const birthday = $('#birthday').val();
 
     try {
-        const docRef = addDoc(collection(db, "clients"), {
+        const addClient = addDoc(collection(db, "clients"), {
             firstName: firstName,
             lastName: lastName,
             age: age,
             birthday: birthday
         });
-        $('#form-success').show();
-        console.log('Se envió')
+        setTimeout(() => {
+            $('#sendButton').html('Usuario creado correctamente')
+            $('#sendButton').css({
+                'background': 'green',
+                'color': 'white',
+                'font-weight': 'bold'
+            })
+            setTimeout(() => {
+                $('#formAddClient').fadeOut(250)
+                getClients()
+            }, 2000)
+        }, 2000);
     } catch (e) {
-        $('#form-danger').show();
+        $('#sendButton').html('Error en la creacion del usuario')
         console.log(e)
     }
 }
 
 $(document).ready(() => {
+    $('#formAddClient').hide()
     getClients()
     $('#addClient').on('submit', addClient);
 })
